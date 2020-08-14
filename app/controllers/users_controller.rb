df1bf3052fd623c5dd5d29c8a1832ac210a1ecc7
萩原翼 <inagakisan.6.2.2.4@ezweb.ne.jp>
2020-08-14 18:10:29 +0000

    8.1.2 終了

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -19,6 +19,19 @@ class UsersController < ApplicationController
     end
   end
   
+  def edit
+    @user = User.find(params[:id])
+  end
+  
+  def update
+    @user = User.find(params[:id])
+    if @user.update_attributes(user_params)
+      # 更新に成功した時の処理を記述します。
+    else
+      render :edit
+    end
+  end
+  
   private
   
     def user_params
